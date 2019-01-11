49416d4e7849bcd364ce70fb7fe08ad8d5fe17e4
Salman-H <sah517@g.harvard.edu>
2019-01-11 07:21:16 -0500

    feat: Implement state prediction step

--- filter_1d.cpp
+++ filter_1d.cpp
@@ -12,3 +12,11 @@ tuple<double, double> measurement_update(double mean1, double var1,
     new_var = 1 / (1 / var1 + 1 / var2);
     return make_tuple(new_mean, new_var);
 }
+
+tuple<double, double> state_prediction(double mean1, double var1,
+                                       double mean2, double var2)
+{
+	new_mean = mean1 + mean2;
+	new_var = var1 + var2;
+	return make_tuple(new_mean, new_var);
+}
